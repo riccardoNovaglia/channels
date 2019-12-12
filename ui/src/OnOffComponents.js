e571dff36f30a7e4a24673aef40b0777f83e335d
Riccardo Novaglia <riccardo.novaglia@gmail.com>
2019-12-12 22:01:04 +0000

    onoff is just a special alternative

--- ui/src/OnOffComponents.js
+++ ui/src/OnOffComponents.js
@@ -1,18 +1,27 @@
 import React from "react";
-import { ChannelContext } from "./App";
+import {
+  QA as AlternativeQa,
+  Dev as AlternativeDev
+} from "./AlternativeComponents";
 
 export function QA(props) {
   return (
-    <ChannelContext.Consumer>
-      {channel => (channel === "qa" || channel === "dev") && props.children}
-    </ChannelContext.Consumer>
+    <AlternativeQa>
+      {{
+        on: props.children,
+        off: <></>
+      }}
+    </AlternativeQa>
   );
 }
 
 export function Dev(props) {
   return (
-    <ChannelContext.Consumer>
-      {context => context === "dev" && props.children}
-    </ChannelContext.Consumer>
+    <AlternativeDev>
+      {{
+        on: props.children,
+        off: <></>
+      }}
+    </AlternativeDev>
   );
 }
